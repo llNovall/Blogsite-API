6bb38d03e5e7f17a519be2cdab1dc2f022ae018d
llNovall <25434267+llNovall@users.noreply.github.com>
2023-10-23 18:18:21 +0200

    Added entities for blog

---
+++ BlogsiteDomain/Entities/AppContent/Project.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogsiteDomain.Entities.AppContent
+{
+    public class Project : BaseEntity
+    {
+        [StringLength(500, MinimumLength = 1)]
+        public string ProjectName { get; set; }
+
+        [StringLength(8000, MinimumLength = 1)]
+        public string ProjectDescription { get; set; }
+
+        [StringLength(500, MinimumLength = 1)]
+        [RegularExpression(@"https{0,1}://.*")]
+        public string ProjectLink { get; set; }
+
+        public Project(string projectName, string projectDescription, string projectLink)
+        {
+            ProjectName = projectName;
+            ProjectDescription = projectDescription;
+            ProjectLink = projectLink;
+        }
+    }
+}
