3dfb37ea63de83edd7c43c36b332b4693fb830bf
llNovall <25434267+llNovall@users.noreply.github.com>
2023-10-23 18:31:41 +0200

    Added context for mongo database

---
+++ BlogsiteMongoAccess/Context/MongoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BlogsiteDomain.Context;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BlogsiteMongoAccess.Context
+{
+    public class MongoDbContext : IMongoDbContext
+    {
+        private readonly IMongoDatabase _db;
+        private readonly MongoCollectionSettings _settings;
+
+        public MongoDbContext(IOptions<MongoConnectionSetting> connectionSetting)
+        {
+            MongoClient client = new(connectionSetting.Value.ConnectionString);
+
+            _db = client.GetDatabase(connectionSetting.Value.DatabaseName);
+
+            _settings = new()
+            {
+                AssignIdOnInsert = true
+            };
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string collectionName) => _db.GetCollection<T>(collectionName, _settings);
+    }
+}
---
+++ BlogsiteMongoAccess/MongoConnectionSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogsiteMongoAccess
+{
+    public class MongoConnectionSetting
+    {
+        public string? ConnectionString { get; set; }
+        public string? DatabaseName { get; set; }
+    }
+}
